d3ab71d19f9b91b48ce9f41b0eae7a52bb8bd5f3
salahuddin <salahuddin934@gmail.com>
2024-03-03 08:00:07 +0530

    dd

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var cookies = require("cookie-parser");
 app.use(cookies());
 
 app.use(cors());
-app.use(express.json({limit: '25mb'}));
+app.use(express.json());
 app.use(express.urlencoded({limit: '25mb', extended: true}));
 
 const server = http.createServer(app);
